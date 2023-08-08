bc49dc13d67aec53bcb3dc801078045527b50d9f
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2023-08-08 18:04:48 +0100

    add fix

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -210,7 +210,7 @@ const Product = () => {
         </div>
         <ReactToPdf targetRef={ref} filename="div-blue.pdf" options={options} x={.5} y={.5} scale={0.8}>
           {({toPdf}: {toPdf: any}) => (
-            <button onClick={toPdf}>Generate pdf</button>
+            <Button className='p-3 m-4 font-medium text-white rounded-lg' onClick={toPdf}>Generate pdf</Button>
           )}
         </ReactToPdf>
         <Button onClick={handleGeneratePdf}>generate pdf 2</Button>
